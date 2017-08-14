0f22ccc5084740b6339be29d17709a376498cdec
Tal Ater <tal@talater.com>
2017-08-14 21:18:07 +0300

    Added Welcome component and route

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Welcome.css';
+
+
+const Welcome = () =>
+  <div id="welcome">
+    Welcome
+  </div>;
+
+export default Welcome;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Map from '../components/Map.jsx';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Map from '../components/Map.jsx';
+import Welcome from '../components/Welcome.jsx';
 
 const App = () =>
   <Router>
-    <Route path="/map" component={ Map } />
+    <div>
+      <Map />
+      <Route path="/welcome" component={ Welcome } />
+    </div>
   </Router>;
 
 export default App;
