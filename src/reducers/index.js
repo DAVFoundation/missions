97ed3a5d484e768648f2e66b0b7ab6f01b3375eb
Tal Ater <tal@talater.com>
2017-08-14 23:06:43 +0300

    Created almost empty reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import settings from './settings';
+
+const reducers = combineReducers({
+  settings
+});
+
+export default reducers;
---
+++ src/reducers/settings.js
@@ -0,0 +1,8 @@
+const settings = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default settings;
