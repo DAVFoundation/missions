1790e9ffe68df75870539c7fb0c843eb246f3b51
Tal Ater <tal@talater.com>
2017-08-14 16:47:28 +0300

    Added react-router-dom to main App

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Map from '../components/Map.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () =>
-  <Map />;
+  <Router>
+    <Route path="/" component={ Map } />
+  </Router>;
 
 export default App;
