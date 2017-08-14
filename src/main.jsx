6074ad21443481124720020076fa47936f9775ab
Tal Ater <tal@talater.com>
2017-08-14 23:06:55 +0300

    Wrapped app in Provider with store from reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducers from './reducers';
 import App from './App.jsx';
 import './main.css';
 
+let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
