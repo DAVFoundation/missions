cd9c8df2bd499498718edbce072a807c83fa09cc
Tal Ater <tal@talater.com>
2017-08-15 22:45:55 +0300

    Renamed configureStore to initStore

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './configureStore';
+import initStore from './initStore';
 import App from './App.jsx';
 import './Main.css';
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
+  <Provider store={initStore()}>
     <App />
   </Provider>,
   document.getElementById('app')
--- src/initStore.js
+++ src/initStore.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux'
 import reducers from './reducers';
 
-const configureStore = () => {
+const initStore = () => {
   let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   return store;
 };
 
-export default configureStore;
+export default initStore;
