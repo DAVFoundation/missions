f112089959f04c422ba65b3b87fbb3e9da009849
Tal Ater <tal@talater.com>
2017-08-17 22:46:04 +0300

    MainScreen now includes logo and user controls (non-functional)

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import './MainScreen.css';
+import logo from '../images/logo_missions.png';
+import userIcon from '../images/user.svg';
 
 class MainScreen extends Component {
   render() {
     return (
-      <div className="screen">MISSIONS powered by DAV</div>
+      <div id="main-screen" className="screen">
+        <div id="logo">
+          <img src={logo} alt="Missions powered by DAV" />
+        </div>
+        <div id="user-controls">
+          <img src={userIcon} alt="User Profile and Settings" />
+        </div>
+      </div>
     );
   }
 }
