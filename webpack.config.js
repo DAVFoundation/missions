9ed307afdc7ae7092c0b349040776299642e19c4
Tal Ater <tal@talater.com>
2017-08-17 22:39:28 +0300

    Webpack's url loader now also handles svg

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'url-loader',
