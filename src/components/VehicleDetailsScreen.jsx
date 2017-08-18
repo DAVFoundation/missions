608767647fe7e0ce9888ee714428737636c69831
Tal Ater <tal@talater.com>
2017-08-19 00:50:09 +0300

    Vehicle details screen now contains a close button

--- src/components/VehicleDetailsScreen.jsx
+++ src/components/VehicleDetailsScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import VehicleDetailsContainer from '../containers/VehicleDetailsContainer.jsx';
 
 const VehicleDetailsScreen = ({match}) => {
   return (
     <div id="vehicle-details-screen" className="screen">
+      <Link to="/">Close</Link>
       <VehicleDetailsContainer vehicleUid={match.params.uid} />
     </div>
   );
