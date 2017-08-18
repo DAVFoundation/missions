782bc418a515d44c24e35760d102caba90b1683a
Tal Ater <tal@talater.com>
2017-08-18 22:43:25 +0300

    Removed MainScreenContainer and simply included MainScreen in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Map from './containers/MapContainer.jsx';
-import MainScreenContainer from './containers/MainScreenContainer.jsx';
+import MainScreen from './components/MainScreen.jsx';
 import Welcome from './components/Welcome.jsx';
 
 const App = () =>
   <Router>
     <div>
       <Map />
-      <Route path="/" component={MainScreenContainer} />
+      <Route path="/" component={ MainScreen } />
       <Route path="/welcome" component={ Welcome } />
     </div>
   </Router>;
