e8567127b676ffbb3f69d5248e3f75e700b821ca
Tal Ater <tal@talater.com>
2017-08-20 14:05:22 +0300

    Added skeleton for an OrderScreen

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Map from './containers/MapContainer.jsx';
 import MainScreen from './components/MainScreen.jsx';
 import VehicleDetailsScreen from './components/VehicleDetailsScreen.jsx';
+import OrderScreen from './components/OrderScreen.jsx';
 import Welcome from './components/Welcome.jsx';
 
 const App = () =>
@@ -16,6 +17,8 @@ const App = () =>
 
       <Route path="/vehicle/:uid" component={ MainScreen } />
       <Route path="/vehicle/:uid" component={ VehicleDetailsScreen } />
+
+      <Route path="/order" component={ OrderScreen } />
 
     </div>
   </Router>;
---
+++ src/components/OrderScreen.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrderScreen extends Component {
+  render() {
+    return (
+      <div id="order-screen" className="screen">
+        Order!
+      </div>
+    );
+  }
+}
+
+export default OrderScreen;
