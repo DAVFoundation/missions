fe6698f2a5a03fa6dc35087b70c65557fddfa6dc
Tal Ater <tal@talater.com>
2017-08-25 11:27:03 +0300

    Refactored App component to a class

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Map from './containers/MapContainer.jsx';
 import MainScreen from './components/MainScreen.jsx';
@@ -6,21 +6,26 @@ import VehicleDetailsScreen from './components/VehicleDetailsScreen.jsx';
 import OrderScreen from './components/OrderScreen.jsx';
 import Welcome from './components/Welcome.jsx';
 
-const App = () =>
-  <Router>
-    <div>
-      <Map />
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Map />
 
-      <Route exact path="/" component={ MainScreen } />
+          <Route exact path="/" component={ MainScreen } />
 
-      <Route path="/welcome" component={ Welcome } />
+          <Route path="/welcome" component={ Welcome } />
 
-      <Route path="/vehicle/:uid" component={ MainScreen } />
-      <Route path="/vehicle/:uid" component={ VehicleDetailsScreen } />
+          <Route path="/vehicle/:uid" component={ MainScreen } />
+          <Route path="/vehicle/:uid" component={ VehicleDetailsScreen } />
 
-      <Route path="/order" component={ OrderScreen } />
+          <Route path="/order" component={ OrderScreen } />
 
-    </div>
-  </Router>;
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
