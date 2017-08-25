c32bb049234de94d9c194f1b71616b8f7c8a0e07
Tal Ater <tal@talater.com>
2017-08-25 11:37:34 +0300

    Update status from server at regular intervals

--- src/lib/app.js
+++ src/lib/app.js
@@ -2,5 +2,9 @@ import store from '../store';
 import { updateStatus } from '../actions';
 
 export function initializeApp() {
+  // Get updated status from server, now and then at steady intervals
   store.dispatch(updateStatus());
+  setInterval(
+    () => store.dispatch(updateStatus())
+    , 2000);
 }
