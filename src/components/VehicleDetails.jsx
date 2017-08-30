fdd4ae6844002c441a3e38c0eee35f91064fae87
Tal Ater <tal@talater.com>
2017-08-30 14:44:23 +0300

    Added details and styles to VehicleDetails

--- src/components/VehicleDetails.jsx
+++ src/components/VehicleDetails.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './VehicleDetails.css';
 
 const VehicleDetails = ({vehicle}) => (
-  <div>
-    VehicleDetails - {vehicle.model}
+  <div className="vehicle-details">
+    <div className="vehicle-card">
+      <img src={vehicle.icon} />
+      <div className="vehicle-vitals">
+        <h2>{vehicle.model}</h2>
+        <div className="dav-uid">{vehicle.id}</div>
+        <div className="rating">Rating <strong>{vehicle.rating}</strong></div>
+      </div>
+    </div>
+    <div className="vehicle-missions-count"><strong>{vehicle.missions_completed} missions</strong> completed successfully</div>
+    <div className="vehicle-missions-count-7"><strong>{vehicle.missions_completed_7_days} missions</strong> completed successfully in last 7 days</div>
   </div>
 );
 
