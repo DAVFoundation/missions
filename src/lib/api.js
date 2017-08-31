c71b7e93327033dd1274635a77253751c04af665
Tal Ater <tal@talater.com>
2017-08-31 15:13:08 +0300

    Added createRequest() function to api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,3 +5,9 @@ export const fetchStatus = ({ id, lat, long }) =>
     .then(
       response => response.json()
     );
+
+export const createRequest = ({user_id, pickup, dropoff, requested_pickup_time, size, weight}) =>
+  fetch(`${apiRoot}/request/new?user_id=${user_id}&pickup=${pickup}&dropoff=${dropoff}&requested_pickup_time=${requested_pickup_time}&size=${size}&weight=${weight}`)
+    .then(
+      response => response.json()
+    );
