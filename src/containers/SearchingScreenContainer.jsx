2202eadb89fed205ecb24667d44ab699830c6f66
Tal Ater <tal@talater.com>
2017-09-01 00:17:48 +0300

    When cancelling searching screen, also call stopSearching action which updates order state to draft

--- src/containers/SearchingScreenContainer.jsx
+++ src/containers/SearchingScreenContainer.jsx
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import { getBidArray } from '../reducers/bids';
 import SearchingScreen from '../components/SearchingScreen.jsx';
+import { updateOrderDetails } from '../actions';
+
+const mapDispatchToProps = (dispatch) => ({
+  stopSearching: () => dispatch(updateOrderDetails({state: 'draft'})),
+});
 
 export default connect(
   (state) => ({
     vehicles: state.vehicles,
     bids: getBidArray(state.bids),
   }),
+  mapDispatchToProps
 )(SearchingScreen);
