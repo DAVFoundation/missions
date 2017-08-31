288beb075c11b4f92e35aa2b45941a0ff1239dd2
Tal Ater <tal@talater.com>
2017-08-31 12:44:29 +0300

    Added bids as props to SearchingScreen

--- src/components/SearchingScreen.jsx
+++ src/components/SearchingScreen.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './SearchingScreen.css';
 import radar from '../images/radar.png';
 
 class SearchingScreen extends Component {
   render() {
+    const { bids } = this.props;
     return (
       <div id="searching-screen" className="screen">
         <h1>Matching you with autonomous vehicles</h1>
         <Link to="/" className="med-button cancel-button">cancel</Link>
         <img src={radar} id="radar" />
+        {bids.map(bid => (
+          <div key={bid.id}>{bid.id}</div>
+        ))}
       </div>
     );
   }
 }
+
+SearchingScreen.propTypes = {
+  vehicles: PropTypes.array.isRequired,
+  bids: PropTypes.array.isRequired,
+};
 
 export default SearchingScreen;
--- src/containers/SearchingScreenContainer.jsx
+++ src/containers/SearchingScreenContainer.jsx
@@ -1,7 +1,11 @@
 import { connect } from 'react-redux';
 import { getVehicleArray } from '../reducers/vehicles';
+import { getBidArray } from '../reducers/bids';
 import SearchingScreen from '../components/SearchingScreen.jsx';
 
 export default connect(
-  (state) => ({vehicles: getVehicleArray(state.vehicles)}),
+  (state) => ({
+    vehicles: getVehicleArray(state.vehicles),
+    bids: getBidArray(state.bids),
+  }),
 )(SearchingScreen);
