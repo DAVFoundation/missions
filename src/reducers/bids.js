16da7bc34a7d29f6ec09dfc5dba9927fd5e26a34
Tal Ater <tal@talater.com>
2017-08-31 12:43:10 +0300

    Added bids reducer

---
+++ src/reducers/bids.js
@@ -0,0 +1,18 @@
+import { handleActions } from 'redux-actions';
+import { updateStatusFulfilled } from '../actions';
+
+const defaultState = {};
+
+export default handleActions({
+
+  [updateStatusFulfilled]: (state, { payload: { bids = [] } }) => {
+    let nextState = {};
+    bids.forEach(bid => {
+      nextState[bid.id] = bid;
+    });
+    return nextState;
+  }
+
+}, defaultState);
+
+export const getBidArray = (state) => Object.entries(state).map(([, bid]) => bid);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import settings from './settings';
 import vehicles from './vehicles';
 import map from './map';
 import order from './order';
+import bids from './bids';
 
 export default combineReducers({
   settings,
   vehicles,
   map,
   order,
+  bids,
 });
