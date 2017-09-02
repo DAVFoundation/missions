28a1ada105a26119d1f90616d96ea246efab5b9f
Tal Ater <tal@talater.com>
2017-09-03 00:22:04 +0300

    Added full details and design to VehicleBid

--- src/components/VehicleBid.jsx
+++ src/components/VehicleBid.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const VehicleBid = ({bid, vehicle, shown}) => {
   let classNames = ['vehicle-bid-card'];
@@ -11,11 +12,19 @@ const VehicleBid = ({bid, vehicle, shown}) => {
       <div className="vehicle-card">
         <img src={vehicle.icon} />
         <div className="vehicle-vitals">
+          <a className="med-button choose-bid-button" onClick={() => console.log(bid.id)}>Order</a>
           <h2>{vehicle.model}</h2>
-          <p>{bid.id}</p>
           <div className="rating">Rating <strong>{vehicle.rating}</strong></div>
         </div>
       </div>
+      <dl className="bid-details">
+        <dt>Estimated pickup time:</dt>
+        <dd>{moment(bid.pickup, 'x').fromNow()}</dd>
+        <dt>Estimated dropoff time:</dt>
+        <dd>{moment(bid.dropoff, 'x').format('h:mm a')}</dd>
+        <dt>Cost for delivery:</dt>
+        <dd>{bid.bid} DAV</dd>
+      </dl>
     </div>
   );
 };
