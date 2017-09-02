13fc0efeca0a809c9cbfcca49228ce9e6b905122
Tal Ater <tal@talater.com>
2017-09-02 14:27:33 +0300

    Show only vehicles with bids on map during `choosing` state as well

--- src/containers/MapContainer.jsx
+++ src/containers/MapContainer.jsx
@@ -14,7 +14,7 @@ export default connect(
     let vehicles = [];
 
     // if we are looking at bids, only show vehicles with bids
-    if (state.order.state === 'searching') {
+    if (['searching', 'choosing'].includes(state.order.state)) {
       getBidArray(state.bids).forEach(
         bid => vehicles.push(state.vehicles[bid.vehicle_id])
       );
