f6d64d389871deaad700e5b0003e397d1098119f
Tal Ater <tal@talater.com>
2017-09-04 20:30:23 +0300

    MainScreen is now wrapped in MainScreenContainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { initializeApp } from './lib/app';
 import Map from './containers/MapContainer.jsx';
-import MainScreen from './components/MainScreen.jsx';
+import MainScreenContainer from './containers/MainScreenContainer.jsx';
 import VehicleDetailsScreen from './components/VehicleDetailsScreen.jsx';
 import OrderScreenContainer from './containers/OrderScreenContainer.jsx';
 import SearchingScreenContainer from './containers/SearchingScreenContainer.jsx';
@@ -20,11 +20,11 @@ class App extends Component {
         <div>
           <Map />
 
-          <Route exact path="/" component={ MainScreen } />
+          <Route exact path="/" component={ MainScreenContainer } />
 
           <Route path="/welcome" component={ Welcome } />
 
-          <Route path="/vehicle/:uid" component={ MainScreen } />
+          <Route path="/vehicle/:uid" component={ MainScreenContainer } />
           <Route path="/vehicle/:uid" component={ VehicleDetailsScreen } />
 
           <Route path="/order" component={ OrderScreenContainer } />
---
+++ src/containers/MainScreenContainer.jsx
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import MainScreen from '../components/MainScreen.jsx';
+import { resetOrderDetails } from '../actions';
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+export default connect(
+  () => {},
+  mapDispatchToProps
+)(MainScreen);
