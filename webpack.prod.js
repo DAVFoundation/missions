c59a964d6e72a491dcc3a8273d71a950500cb3aa
Tal Ater <tal@talater.com>
2017-09-04 14:26:04 +0300

    Temporarily removed UglifyJS from webpack production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,12 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new UglifyJSPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
