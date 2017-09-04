39f045722204d10062719a37741b745358eff8f5
Tal Ater <tal@talater.com>
2017-09-04 16:26:20 +0300

    Removed user controls from MainScreen

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './MainScreen.css';
 import logo from '../images/logo_missions.png';
-import userIcon from '../images/user.svg';
 
 class MainScreen extends Component {
   render() {
@@ -10,9 +9,6 @@ class MainScreen extends Component {
       <div id="main-screen" className="screen">
         <div id="logo">
           <img src={logo} alt="Missions powered by DAV" />
-        </div>
-        <div id="user-controls">
-          <img src={userIcon} alt="User Profile and Settings" />
         </div>
         <Link to="/order" className="big-button order-button">Order Pickup</Link>
       </div>
