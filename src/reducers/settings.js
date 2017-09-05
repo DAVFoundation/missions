7a408f3f0dc41c8a4b115e7b44ab55519079a2c9
Tal Ater <tal@talater.com>
2017-09-06 00:42:37 +0300

    Added user_icon to settings store

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,6 +1,7 @@
 const defaultState = {
   'welcome_seen': false, // Has user finished/skipped/closed on-boarding process?
   'user_id': '0xbf6650a594ca39ae373b7800a2c0f617ae8c81d3',
+  'user_icon': 'https://lorempixel.com/100/100/abstract/',
 };
 
 export default (state = defaultState, action) => {
