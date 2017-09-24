9cb8ca1b101a9fdb8685ed1957698153d9e1a1c8
Tal Ater <tal@talater.com>
2017-09-24 18:51:40 +0200

    Include user id in calls to get status

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,19 +4,21 @@ import { getShortCoordsString } from '../lib/utils';
 const apiRoot = process.env.MISSION_CONTROL_HOST;
 
 export const fetchStatus = ({ id, lat, long, requestId }) => {
+  const userId = store.getState().settings.user_id;
   let url = new URL(`/status`, apiRoot);
   id && url.searchParams.set('id', id);
   lat && url.searchParams.set('lat', lat); // Don't stand on the equator or you'll break this
   long && url.searchParams.set('long', long);
   requestId && url.searchParams.set('requestId', requestId);
+  url.searchParams.set('userId', userId);
   return fetch(url)
     .then(response => response.json());
 };
 
 export const createRequest = ({pickup, dropoff, requested_pickup_time, size, weight}) => {
+  const userId = store.getState().settings.user_id;
   pickup = getShortCoordsString(pickup, 8, ',');
   dropoff = getShortCoordsString(dropoff, 8, ',');
-  const userId = store.getState().settings.user_id;
   return fetch(`${apiRoot}/request/new?user_id=${userId}&pickup=${pickup}&dropoff=${dropoff}&requested_pickup_time=${requested_pickup_time}&size=${size}&weight=${weight}`)
     .then(response => response.json());
 };
