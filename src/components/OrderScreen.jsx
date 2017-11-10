739df5be8173c3c23369d587a1bc1e5c6ffaa9d0
nicolasdelfino <nicolas.delfino@tretton37.com>
2017-11-10 11:31:48 +0100

    Prep order screen for package size component

--- src/components/OrderScreen.jsx
+++ src/components/OrderScreen.jsx
@@ -4,12 +4,19 @@ import { Link } from 'react-router-dom';
 import { getShortCoordsString, coordsFromString } from '../lib/utils';
 import './OrderScreen.css';
 import arrow from '../images/arrow-left.svg';
+import PackageOptions from './PackageOptions.jsx';
 
 class OrderScreen extends Component {
   constructor(props) {
     super(props);
     this.updateStoreFromForm = this.updateStoreFromForm.bind(this);
     this.submitForm = this.submitForm.bind(this);
+
+    this.state = {
+      packageSize: ''
+    };
+
+    this.selectPackageSize = this.selectPackageSize.bind(this);
   }
 
   componentDidMount() {
@@ -20,30 +27,47 @@ class OrderScreen extends Component {
     return {
       pickup: coordsFromString(this.pickupNode.value),
       dropoff: coordsFromString(this.dropoffNode.value),
-      size: this.sizeNode.value || undefined,
+      size: this.state.packageSize || undefined,
       weight: this.weightNode.value || undefined,
-      requested_pickup_time: this.pickupTimeNode.value || undefined,
+      requested_pickup_time: this.pickupTimeNode.value || undefined
     };
   }
 
   updateStoreFromForm(detailOverride = {}) {
     const details = this.createOrderDetailsObject();
-    this.props.updateOrderDetails({...details, ...detailOverride});
+    this.props.updateOrderDetails({ ...details, ...detailOverride });
   }
 
   submitForm() {
     const { userCoords, createRequest } = this.props;
-    this.updateStoreFromForm({stage: 'searching'});
+    this.updateStoreFromForm({ stage: 'searching' });
     let requestDetails = this.createOrderDetailsObject();
-    requestDetails.pickup = requestDetails.pickup || {lat: userCoords.lat, long: userCoords.long};
+    requestDetails.pickup = requestDetails.pickup || {
+      lat: userCoords.lat,
+      long: userCoords.long
+    };
     createRequest(requestDetails);
   }
 
+  selectPackageSize(size) {
+    console.log('select', size);
+    if (this.state.packageSize !== size) {
+      this.setState({ packageSize: size });
+    } else {
+      this.setState({ packageSize: undefined });
+    }
+  }
+
+  getSizeContainer() {}
+
   render() {
-    const { userCoords, pickup, dropoff, size, weight } = this.props;
-    const requested_pickup_time = this.props.requested_pickup_time || (new Date).toTimeString().slice(0,5);
+    const { userCoords, pickup, dropoff, weight } = this.props; // size
+    const requested_pickup_time =
+      this.props.requested_pickup_time || new Date().toTimeString().slice(0, 5);
     const userCoordsString = getShortCoordsString(userCoords);
-    const pickupPlaceholder = userCoordsString ? `Your current location (${userCoordsString})` : '';
+    const pickupPlaceholder = userCoordsString
+      ? `Your current location (${userCoordsString})`
+      : '';
     const pickupCoordsString = getShortCoordsString(pickup);
     const dropoffCoordsString = getShortCoordsString(dropoff);
     return (
@@ -51,46 +75,72 @@ class OrderScreen extends Component {
         <Link to="/" className="back-button" onClick={this.updateStoreFromForm}>
           <img src={arrow} alt="Back" />
         </Link>
-
         <h1>Order Pickup</h1>
-
         <div className="form-field">
           <label htmlFor="pickup-location">Set pickup location</label>
-          <input type="text" id="pickup-location" placeholder={pickupPlaceholder} defaultValue={pickupCoordsString} ref={node => { this.pickupNode = node; }} />
+          <input
+            type="text"
+            id="pickup-location"
+            placeholder={pickupPlaceholder}
+            defaultValue={pickupCoordsString}
+            ref={node => {
+              this.pickupNode = node;
+            }}
+          />
         </div>
-
         <div className="form-field">
           <label htmlFor="dropoff-location">Set dropoff location</label>
-          <input type="text" id="dropoff-location" defaultValue={dropoffCoordsString} ref={node => { this.dropoffNode = node; }} />
+          <input
+            type="text"
+            id="dropoff-location"
+            defaultValue={dropoffCoordsString}
+            ref={node => {
+              this.dropoffNode = node;
+            }}
+          />
         </div>
 
         <div className="form-field">
-          <label htmlFor="package-size">How big is the package?</label>
-          <select id="package-size" defaultValue={size} ref={node => { this.sizeNode = node; }}>
-            <option value="letter">Letter</option>
-            <option value="can">Beverage can</option>
-            <option value="pizza">Pizza box</option>
-            <option value="box">Shipping box</option>
-          </select>
+          <label>How big is the package?</label>
+          <PackageOptions
+            onSelect={this.selectPackageSize}
+            selectedOption={this.state.packageSize}
+          />
         </div>
 
         <div className="form-field">
           <label htmlFor="weight">Weight</label>
-          <select id="weight" defaultValue={weight} ref={node => { this.weightNode = node; }}>
+          <select
+            id="weight"
+            defaultValue={weight}
+            ref={node => {
+              this.weightNode = node;
+            }}
+          >
             <option value="500">Up to 500 grams</option>
             <option value="1000">Up to 1 kg</option>
             <option value="5000">Up to 5 kg</option>
             <option value="100000">Up to 100 kg</option>
           </select>
         </div>
-
         <div className="form-field">
           <label htmlFor="pickup-time">Pickup time</label>
-          <input id="pickup-time" type="time" defaultValue={requested_pickup_time} ref={node => { this.pickupTimeNode = node; }} />
+          <input
+            id="pickup-time"
+            type="time"
+            defaultValue={requested_pickup_time}
+            ref={node => {
+              this.pickupTimeNode = node;
+            }}
+          />
         </div>
-
-        <Link to="/searching" className="big-button form-submit-button" onClick={this.submitForm}>Find drones</Link>
-
+        <Link
+          to="/searching"
+          className="big-button form-submit-button"
+          onClick={this.submitForm}
+        >
+          Find drones
+        </Link>
       </div>
     );
   }
@@ -105,7 +155,7 @@ OrderScreen.propTypes = {
   weight: PropTypes.string,
   updateOrderDetails: PropTypes.func.isRequired,
   createRequest: PropTypes.func.isRequired,
-  onMount: PropTypes.func.isRequired,
+  onMount: PropTypes.func.isRequired
 };
 
 export default OrderScreen;
