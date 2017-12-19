e193a6ad81be63b90606201bcfaf602be5d19fd6
Tal Ater <tal@talater.com>
2017-12-19 18:36:12 +0200

    Tweaked default dropoff coordinates

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const config = {
-  default_dropoff_coordinates: {lat: 55.704265, long: 13.185997},
+  default_dropoff_coordinates: {lat: 32.087450, long: 34.789238},
   default_package_size: 'letter',
   default_package_weight: '500'
 };
-    
+
 export default (key) => {
   if (!(key in config)) {
     throw `The given key "${key}" to config was invalid`;
@@ -11,4 +11,3 @@ export default (key) => {
     return config[key];
   }
 };
-  
