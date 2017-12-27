f9d13d1f247cd0dde10a4fdb504dbf9d4911ac70
Griffin Obeid <griffobeid@gmail.com>
2017-12-27 12:24:07 -0500

    Finished setting up local storage for the user settings.

--- src/store.js
+++ src/store.js
@@ -2,12 +2,21 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import promiseMiddleware from 'redux-promise-middleware';
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage.js';
 
 const initStore = () => {
+  const persistedState = loadState();
   const store = createStore(
     reducers,
+    persistedState,
     composeWithDevTools(applyMiddleware(promiseMiddleware()))
   );
+
+  store.subscribe(() => {
+    saveState({
+      settings: store.getState().settings
+    });
+  });
 
   return store;
 };
