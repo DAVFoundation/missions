0c6a14a851b1fdd15035fb5d9b3fdbc38f9dfb8f
Timi Ajiboye <trypio00@gmail.com>
2018-01-06 05:40:54 +0100

    feat: completed UI flow for awaiting pickup/confirm takeoff screen

--- src/components/ConfirmTakeoff.jsx
+++ src/components/ConfirmTakeoff.jsx
@@ -1,46 +1,64 @@
 import React, {Component} from 'react';
 import './ConfirmTakeoff.css';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
 
 class ConfirmTakeoff extends Component {
 
+  constructor () {
+    super();
+    this.state = {
+      alertHidden: true
+    };
+  }
+
+  toggleAlert () {
+    this.setState({
+      alertHidden: !this.state.alertHidden
+    });
+  }
+
   render() {
-    return (
-      <div id="confirm-takeoff-screen" className="screen">
+    return (this.state.alertHidden && (<div id="confirm-takeoff-screen" className="screen">
+      <div className="screen-background--dark"/>
+      <div className="modal-container">
+        <div className="modal-box confirm-takeoff">
+          <h1>Ready for Pickup</h1>
+          <p>Drone has arrived at <br/><b>6.497836, 3.3830303</b></p>
+          <p>
+            <i>Please load the package into the yellow compartment on the bottom of the drone,
+              close the door with the latch until you hear a click.</i>
+          </p>
+          <button onClick={this.toggleAlert.bind(this)} className="big-button with-subtext">
+            TAKE OFF<br/>
+            <span className="button-subtext">PACKAGE LOADED AND SECURE</span>
+          </button>
+        </div>
+      </div>
+    </div>))
+      || !this.state.alertHidden && (<div id="confirm-takeoff-screen" className="screen">
         <div className="screen-background--dark"/>
         <div className="modal-container">
-          <div className="modal-box confirm-takeoff">
-            <h1>Ready for Pickup</h1>
-            <p>Drone has arrived at <br/><b>6.497836, 3.3830303</b></p>
-            <p>
-              <i>Please load the package into the yellow compartment on the bottom of the drone,
-                close the door with the latch until you hear a click.</i>
-            </p>
-            <button className="big-button with-subtext">
-              TAKE OFF<br/>
-              <span className="button-subtext">PACKAGE LOADED AND SECURE</span>
-            </button>
-          </div>
-
-          <div hidden className="modal-box alert-box confirm-takeoff">
+          <div className="modal-box alert-box confirm-takeoff">
             <h1>Drone will take off immediately</h1>
             <p>Are you sure the package is secure and the area around the drone is clear?</p>
             <div className="alert-button-container">
-              <button className="alert-button-cancel">
+              <button onClick={this.toggleAlert.bind(this)} className="alert-button-cancel">
                 CANCEL
               </button>
-              <button className="alert-button-confirm">
+              <button onClick={this.props.confirmTakeoff} className="alert-button-confirm">
                 CONFIRM
               </button>
             </div>
-
           </div>
         </div>
-      </div>
-
-
-    );
+      </div>);
   }
 }
+
+ConfirmTakeoff.propTypes = {
+  confirmTakeoff: PropTypes.func.isRequired
+};
+
 
 export default ConfirmTakeoff;
--- src/containers/ConfirmTakeoffContainer.jsx
+++ src/containers/ConfirmTakeoffContainer.jsx
@@ -1,6 +1,12 @@
 import { connect } from 'react-redux';
 import ConfirmTakeoff from '../components/ConfirmTakeoff.jsx';
+import { confirmTakeoff } from '../actions';
+
+const mapDispatchToProps = (dispatch) => ({
+  confirmTakeoff: () => dispatch(confirmTakeoff({}))
+});
 
 export default connect(
-  () => ({})
+  null,
+  mapDispatchToProps
 )(ConfirmTakeoff);
