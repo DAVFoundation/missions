57da92ff2cde6513c81dc7d72dcca82979456e4a
Tal Ater <tal@talater.com>
2018-02-05 15:28:09 +0200

    MapContainer extracted matchStateToProps code to function

--- src/containers/MapContainer.jsx
+++ src/containers/MapContainer.jsx
@@ -5,35 +5,37 @@ import { getBidArray } from '../reducers/bids';
 import { updateMapCoords } from '../actions';
 import Map from '../components/Map.jsx';
 
+const matchStateToProps = (state) => {
+  let vehicles = [];
+  // if we are looking at bids, only show vehicles with bids
+  if (['searching', 'choosing'].includes(state.order.stage)) {
+    getBidArray(state.bids).forEach(
+      bid => state.vehicles[bid.vehicle_id] && vehicles.push(state.vehicles[bid.vehicle_id])
+    );
+  } else {
+    vehicles = getVehicleArray(state.vehicles);
+  }
+  let props = {
+    vehicles,
+    orderStage: state.order.stage,
+    pickup: state.order.pickup,
+    dropoff: state.order.dropoff
+  };
+  if (state.mission) {
+    props.missionStatus = state.mission.status;
+    if (props.missionStatus === 'in_progress'){
+      props.dropoff = {long: state.mission.dropoff_long, lat:state.mission.dropoff_lat};
+      props.pickup = {long: state.mission.pickup_long, lat: state.mission.pickup_lat};
+    }
+  }
+  return props;
+};
+
 const mapDispatchToProps = (dispatch) => ({
   onMoveEnd: (coords) => dispatch(updateMapCoords({coords: coords}))
 });
 
 export default connect(
-  (state) => {
-    let vehicles = [];
-    // if we are looking at bids, only show vehicles with bids
-    if (['searching', 'choosing'].includes(state.order.stage)) {
-      getBidArray(state.bids).forEach(
-        bid => state.vehicles[bid.vehicle_id] && vehicles.push(state.vehicles[bid.vehicle_id])
-      );
-    } else {
-      vehicles = getVehicleArray(state.vehicles);
-    }
-    let props = {
-      vehicles,
-      orderStage: state.order.stage,
-      pickup: state.order.pickup,
-      dropoff: state.order.dropoff
-    };
-    if (state.mission) {
-      props.missionStatus = state.mission.status;
-      if (props.missionStatus === 'in_progress'){
-        props.dropoff = {long: state.mission.dropoff_long, lat:state.mission.dropoff_lat};
-        props.pickup = {long: state.mission.pickup_long, lat: state.mission.pickup_lat};
-      }
-    }
-    return props;
-  },
+  matchStateToProps,
   mapDispatchToProps
 )(withRouter(Map));
