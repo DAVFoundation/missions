be1541630f44a40199caa886a7571eb312aae25f
Paul <pfreema1@gmail.com>
2018-02-14 10:27:57 -0500

    sort options ui finished

--- src/components/BidSelectionHeader.jsx
+++ src/components/BidSelectionHeader.jsx
@@ -4,34 +4,44 @@ import './BidSelectionHeader.css';
 import sort_button from '../images/sort_button.svg';
 import arrow_left from '../images/arrow-left.svg';
 import x_button from '../images/x_button.svg';
-// import check from '../images/check.svg';
+import check from '../images/check.svg';
 
 class BidSelectionHeader extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      sortOptionsOpen: false
+      sortOptionsOpen: false,
+      sortOptionSelected: 'bestMatch'
     };
 
     this.handleSortButtonClick = this.handleSortButtonClick.bind(this);
     this.handleCloseButtonClick = this.handleCloseButtonClick.bind(this);
+    this.handleSortOptionClick = this.handleSortOptionClick.bind(this);
   }
 
   handleSortButtonClick() {
-    console.log('sort button clicked');
-
     this.setState({ sortOptionsOpen: !this.state.sortOptionsOpen });
   }
 
   handleCloseButtonClick() {
-    console.log('close button clicked');
-
     this.setState({ sortOptionsOpen: !this.state.sortOptionsOpen });
   }
 
+  handleSortOptionClick(e) {
+    console.log(e.target.id);
+
+    //call function in SearchingScreen, passing in
+    //selected sort option
+
+    this.setState({
+      sortOptionSelected: e.target.id,
+      sortOptionsOpen: !this.state.sortOptionsOpen
+    });
+  }
+
   render() {
-    let { sortOptionsOpen } = this.state;
+    let { sortOptionsOpen, sortOptionSelected } = this.state;
     return (
       <div className="bid-selection-header-wrapper">
         <div
@@ -47,10 +57,90 @@ class BidSelectionHeader extends Component {
           </div>
           <div className="sort-options__header">Sort by:</div>
           <ul className="sort-options__list">
-            <li>Best match</li>
-            <li>Fastest pickup</li>
-            <li>Fastest delivery</li>
-            <li>Lowest cost</li>
+            <li
+              title="Best match"
+              id="bestMatch"
+              className={
+                sortOptionSelected === 'bestMatch'
+                  ? 'sort-options__list--selected'
+                  : ''
+              }
+              onClick={this.handleSortOptionClick}
+            >
+              Best match
+              {sortOptionSelected === 'bestMatch' ? (
+                <img src={check} alt="checkmark" />
+              ) : (
+                <img
+                  className="sort-options__checkmark--hide"
+                  src={check}
+                  alt="checkmark"
+                />
+              )}
+            </li>
+            <li
+              title="Fastest pickup"
+              id="fastestPickup"
+              className={
+                sortOptionSelected === 'fastestPickup'
+                  ? 'sort-options__list--selected'
+                  : ''
+              }
+              onClick={this.handleSortOptionClick}
+            >
+              Fastest pickup
+              {sortOptionSelected === 'fastestPickup' ? (
+                <img src={check} alt="checkmark" />
+              ) : (
+                <img
+                  className="sort-options__checkmark--hide"
+                  src={check}
+                  alt="checkmark"
+                />
+              )}
+            </li>
+            <li
+              title="Fastest delivery"
+              id="fastestDelivery"
+              className={
+                sortOptionSelected === 'fastestDelivery'
+                  ? 'sort-options__list--selected'
+                  : ''
+              }
+              onClick={this.handleSortOptionClick}
+            >
+              Fastest delivery
+              {sortOptionSelected === 'fastestDelivery' ? (
+                <img src={check} alt="checkmark" />
+              ) : (
+                <img
+                  className="sort-options__checkmark--hide"
+                  src={check}
+                  alt="checkmark"
+                />
+              )}
+            </li>
+            <li
+              title="Lowest cost"
+              id="lowestCost"
+              className={
+                sortOptionSelected === 'lowestCost'
+                  ? 'sort-options__list--selected'
+                  : ''
+              }
+              onClick={this.handleSortOptionClick}
+            >
+              Lowest cost
+              {sortOptionSelected === 'lowestCost' ? (
+                <img src={check} alt="checkmark" />
+              ) : (
+                <img
+                  className="sort-options__checkmark--hide"
+                  src={check}
+                  alt="checkmark"
+                />
+              )}
+            </li>
           </ul>
         </div>
         <div className="bid-selection-header">
