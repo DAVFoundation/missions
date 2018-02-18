96391aaf5b74bd8527e1d4acb47afb6b48139303
Tal Ater <tal@talater.com>
2018-02-18 11:17:19 +0200

    Prettified tests

--- test/actions/updateApp.spec.js
+++ test/actions/updateApp.spec.js
@@ -3,7 +3,7 @@ import { updateApp } from '../../src/actions';
 describe('updateApp action', () => {
   it('should create an action with type UPDATE_APP', () => {
     const expectedAction = {
-      type: 'UPDATE_APP'
+      type: 'UPDATE_APP',
     };
     expect(updateApp()).toEqual(expectedAction);
   });
