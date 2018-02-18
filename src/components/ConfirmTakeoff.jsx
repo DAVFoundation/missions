c8667e1d83d27dc436e9f9d14a3b908dae163b13
srfrnk <srfrnk@gmail.com>
2018-02-18 13:30:13 +0200

    fix(code style): supposed to prevent indentation error and be more clear to read

--- src/components/ConfirmTakeoff.jsx
+++ src/components/ConfirmTakeoff.jsx
@@ -32,39 +32,45 @@ class ConfirmTakeoff extends Component {
   }
 
   render() {
-    return (this.state.alertHidden && (<div id="confirm-takeoff-screen" className="screen">
-      <div className="screen-background--dark"/>
-      <div className="modal-container">
-        <div className="modal-box confirm-takeoff">
-          <h1>Ready for Pickup</h1>
-          <p>Drone has arrived at <br/><b>{this.props.coords.lat}, {this.props.coords.long}</b></p>
-          <p>
-            <i>Please slide the package into the compartment at the bottom of the drone. Package should click into place when secured.</i>
-          </p>
-          <button onClick={this.toggleAlert.bind(this)} className="big-button with-subtext">
-            TAKE OFF<br/>
-            <span className="button-subtext">PACKAGE LOADED AND SECURE</span>
-          </button>
-        </div>
-      </div>
-    </div>))
-      || !this.state.alertHidden && (<div id="confirm-takeoff-screen" className="screen">
-      <div className="screen-background--dark"/>
-      <div className="modal-container">
-        <div className="modal-box alert-box confirm-takeoff">
-          <h1>Drone will take off immediately</h1>
-          <p>Are you sure the package is secure and the area around the drone is clear?</p>
-          <div className="alert-button-container">
-            <button onClick={this.toggleAlert.bind(this)} className="alert-button-cancel" disabled={this.state.buttonsDisabled}>
-              CANCEL
-            </button>
-            <button onClick={this.props.confirmTakeoff} className="alert-button-confirm" disabled={this.state.buttonsDisabled}>
-              CONFIRM
-            </button>
+    if(this.state.alertHidden) {
+      return (
+        <div id="confirm-takeoff-screen" className="screen">
+          <div className="screen-background--dark"/>
+          <div className="modal-container">
+            <div className="modal-box confirm-takeoff">
+              <h1>Ready for Pickup</h1>
+              <p>Drone has arrived at <br/><b>{this.props.coords.lat}, {this.props.coords.long}</b></p>
+              <p>
+                <i>Please slide the package into the compartment at the bottom of the drone. Package should click into place when secured.</i>
+              </p>
+              <button onClick={this.toggleAlert.bind(this)} className="big-button with-subtext">
+                TAKE OFF<br/>
+                <span className="button-subtext">PACKAGE LOADED AND SECURE</span>
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>);
+        </div>);
+    }
+    else {
+      return (
+        <div id="confirm-takeoff-screen" className="screen">
+          <div className="screen-background--dark"/>
+          <div className="modal-container">
+            <div className="modal-box alert-box confirm-takeoff">
+              <h1>Drone will take off immediately</h1>
+              <p>Are you sure the package is secure and the area around the drone is clear?</p>
+              <div className="alert-button-container">
+                <button onClick={this.toggleAlert.bind(this)} className="alert-button-cancel" disabled={this.state.buttonsDisabled}>
+                  CANCEL
+                </button>
+                <button onClick={this.props.confirmTakeoff} className="alert-button-confirm" disabled={this.state.buttonsDisabled}>
+                  CONFIRM
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>);
+    }
   }
 }
 
