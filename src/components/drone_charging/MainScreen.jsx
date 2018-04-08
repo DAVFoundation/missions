dc2fb6707ee3243ef68053b274d9fae1a19dac8c
Timi Ajiboye <timi@helloworld.ng>
2018-04-08 18:54:13 +0100

    chore: added user profile icon and user location text labels

--- src/components/drone_charging/MainScreen.jsx
+++ src/components/drone_charging/MainScreen.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Link from '../../containers/LinkContainer.jsx';
 import '../MainScreen.css';
 import logo from '../../images/logo_missions.svg';
+import iconUser from '../../images/icon_signed_out.svg';
 
 
 class MainScreen extends Component {
@@ -17,6 +18,14 @@ class MainScreen extends Component {
             <img src={logo} alt="Missions powered by DAV" />
           </div>
           <span className="logo-subtext">4 charging stations available in viewable area</span>
+          <span className="profile">
+            <img src={iconUser} alt=""/>
+          </span>
+        </div>
+
+        <div className="user-location">
+          <h3>Your Location</h3>
+          <p>1556 Broadway, suite 416</p>
         </div>
         <Link to="/order" className="big-button order-button">Find Charging Stations</Link>
       </div>
