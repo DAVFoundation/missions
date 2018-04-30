1283775eef1f91437f913003d8f2139bd55d831e
Anatoly T <antt001@gmail.com>
2018-04-30 17:02:41 +0300

    fix: needs for route plan and charge api calls fulfiled actions

--- src/lib/api.js
+++ src/lib/api.js
@@ -26,27 +26,24 @@ export const createDroneChargingNeed = ({ chargingVelocity, currentCharge, drone
   // :    { address: "TT 453A Giải Phóng, Phương Liệt, Thanh Xuân, Hanoi, Vietnam", lat: 20.995084, long: 105.84166400000004 }
   const params = {
     chargingVelocity, currentCharge, droneLocation, droneType, searchRadius,
+    need_location_latitude: droneLocation.lat,
+    need_location_longitude: droneLocation.long,
     need_type: NEED_TYPES.DRONE_CHARGING
   };
 
   return createNeed(params);
 };
 
-export const createRoutePlanNeed = ({ pickup, dropoff, pickup_at, size, weight }) => {
-  pickup_at = moment(pickup_at, 'HH:mm').format('x');
-  const sizeOption = packageSizeOptions.find(
-    sizeOption => sizeOption.id === size,
-  );
-
+export const createRoutePlanNeed = ({ startPosition, endPosition, flightHeight, heightUnits }) => {
   const params = {
-    pickup_at: pickup_at,
-    pickup_latitude: pickup.lat,
-    pickup_longitude: pickup.long,
-    pickup_address: pickup.address,
-    dropoff_latitude: dropoff.lat,
-    dropoff_longitude: dropoff.long,
-    cargo_type: sizeOption.cargoType,
-    weight: parseFloat(weight),
+    need_location_latitude: startPosition.lat,
+    need_location_longitude: startPosition.long,
+    start_latitude: startPosition.lat,
+    start_longitude: startPosition.long,
+    end_latitude: endPosition.lat,
+    end_longitude: endPosition.long,
+    flight_height: flightHeight,
+    height_units: heightUnits,
     need_type: NEED_TYPES.ROUTE_PLAN
   };
 
@@ -57,7 +54,7 @@ export const createNeed = (params) => {
   let url = new URL(`/needs`, apiRoot);
   params = {
     ...params,
-    need_type: params.need_type
+    // need_type: params.need_type
   };
   return fetchWithUserId(url, 'POST', params);
 };
@@ -69,6 +66,8 @@ export const createDroneDeliveryNeed = ({ pickup, dropoff, pickup_at, size, weig
   );
 
   const params = {
+    need_location_latitude: pickup.lat,
+    need_location_longitude: pickup.long,
     pickup_at: pickup_at,
     pickup_latitude: pickup.lat,
     pickup_longitude: pickup.long,
